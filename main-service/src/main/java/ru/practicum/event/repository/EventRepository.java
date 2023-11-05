d7768a1a16603617c07c57b74c617b18d77239e5
Ivan <solarmaltsev@yandex.ru>
2023-11-05 10:28:40 +0300

    main_svc

--- main-service/src/main/java/ru/practicum/event/repository/EventRepository.java
+++ main-service/src/main/java/ru/practicum/event/repository/EventRepository.java
@@ -3,11 +3,8 @@ package ru.practicum.event.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import ru.practicum.event.dto.EventShortResponseDto;
 import ru.practicum.event.entity.Event;
 
 import java.util.Optional;
@@ -15,13 +12,6 @@ import java.util.Set;
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, Long>, QuerydslPredicateExecutor<Event> {
-
-    @Query("SELECT new ru.practicum.dto.EventShortResponseDto(e.id, e.title, e.description, e.startDate, e.endDate, e.views, COUNT(cr)) " +
-            "FROM Event e " +
-            "LEFT JOIN e.confirmedRequests cr " +
-            "WHERE e.initiatorId = :userId " +
-            "GROUP BY e.id")
-    Page<EventShortResponseDto> findEventsWithViewsAndConfirmedRequestsByUserId(@Param("userId") long userId, Pageable pageable);
 
     Page<Event> findAllByInitiatorId(long userId, Pageable pageable);
 
--- main-service/src/main/java/ru/practicum/event/service/EventServiceImpl.java
+++ main-service/src/main/java/ru/practicum/event/service/EventServiceImpl.java
@@ -98,9 +98,10 @@ public class EventServiceImpl implements EventService {
     @Transactional(readOnly = true)
     public List<EventShortResponseDto> getEvents(long userId, int from, int size) {
         Pageable pageable = PageRequest.of(from / size, size, Sort.by("id"));
-        Page<EventShortResponseDto> events = eventRepository.findEventsWithViewsAndConfirmedRequestsByUserId(userId, pageable);
-        log.info("{} events found by request", events.getTotalElements());
-        return events.getContent();
+        Page<Event> events = eventRepository.findAllByInitiatorId(userId, pageable);
+        List<EventShortResponseDto> result = updateViewsAndConfirmedRequestsInEventShortResponseDto(events.toList());
+        log.info("{} events found by request", result.size());
+        return result;
     }
 
     @Override
